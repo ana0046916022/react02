449b59c7b8f5f2e0de37f52291faa2e2f2882934
anamamani <anamamaniparo@gmail.com>
2023-10-17 19:26:39 -0600

    instale bootwatch,m-icons,toastify

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+//import './App.css';
+import C01componente from './pagina/C01componente';
+
+function App() {
+  return (
+    <div>
+      Hola mundo desde React...
+      <i class="large material-icons">insert_chart</i>
+      <C01componente/>
+    </div>
+  );
+}
+
+export default App;
