61e4347492eabc16e775611b7564251a9965ff06
anamamani <anamamaniparo@gmail.com>
2023-10-18 18:28:19 -0600

    v5.diseño de Aap,Appform

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import AppForm from './components/AppForm';
 import logo from './logo.svg';
 //import './App.css';
 import C01componente from './pagina/C01componente';
 
 function App() {
   return (
-    <div>
-      Hola mundo desde React...
-      <i class="large material-icons">insert_chart</i>
-      <C01componente/>
+    <div style={{width:"350px", background:"greenyellow", padding:"10px"}}>
+      <AppForm />
+      <p>No. 1 Ana Maria      X   A</p>
+      <p>No. 2 Luz Karina      X   A</p>
     </div>
   );
 }
